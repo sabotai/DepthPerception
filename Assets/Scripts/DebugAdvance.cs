a0a13230afd72566bc472241cafc400a3ab416a4
sabotai <sabotaione@gmail.com>
2015-04-12 23:09:04 -0400

    changes made while in actual 1208

---
+++ Assets/Scripts/DebugAdvance.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DebugAdvance : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Backspace)){
+			GameObject fader = GameObject.Find("guiFader");
+			fader.GetComponent<ScreenFadeInOut>().fadeOut = true;
+		}
+
+	}
+}
