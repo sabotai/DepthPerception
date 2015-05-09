7747a93c160eb00951b7ca7846c0cc69ec36a328
Alec McClure <sabotaione@gmail.com>
2015-05-09 02:10:46 -0400

    added udp stuff for large systems

---
+++ Assets/Scripts/OSCSend.cs
@@ -0,0 +1,72 @@
+﻿
+//script adapted from http://gameaudiomiddleware.tumblr.com/post/21680680198/inside-unity3d-1-udp-network
+
+
+
+using UnityEngine;
+using System.Collections;
+using System.Net.Sockets;
+using System.Text;
+using System.Collections;
+
+public class OSCSend : MonoBehaviour {
+
+	private UdpClient clientP = new UdpClient();
+	private ASCIIEncoding byteEncoder = new ASCIIEncoding();
+
+	// Use this for initialization
+	void Start () {
+		//clientP.Connect("127.0.0.1", 6666);
+		clientP.Connect ("255.255.255.255", 6666);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float xPercent = 100 * (Input.mousePosition.x / Screen.width);
+		float yPercent = 100-(100 * (Input.mousePosition.y / Screen.height));
+
+		SendInt ("mouseX:", (int)xPercent);
+		SendInt ("mouseY:", (int)yPercent);
+		Debug.Log ("xpercent = " + xPercent + "  ypercent = " + yPercent);
+
+		if (Input.inputString != ""){
+			SendString ("key:" + Input.inputString);
+		}
+		
+		if (Input.GetKey(KeyCode.UpArrow)){
+			SendString("dir:" + "UP");
+		}
+		if (Input.GetKey(KeyCode.DownArrow)){
+			SendString("dir:" + "DOWN");
+		}
+		if (Input.GetKey(KeyCode.LeftArrow)){
+			SendString("dir:" + "LEFT");
+		}
+		if (Input.GetKey(KeyCode.RightArrow)){
+			SendString("dir:" + "RIGHT");
+		}
+	}
+	
+	public void SendInt(string routeString, int value){
+		string message = routeString + " " + value;
+		byte[] messageBytes = byteEncoder.GetBytes(message);
+		SendBytes(messageBytes);
+		
+	}
+	public void SendString(string routeString){
+		string message = routeString;
+		byte[] messageBytes = byteEncoder.GetBytes(message);
+		SendBytes(messageBytes);
+		
+	}
+
+	public void SendBytes(byte[] message){
+		try {
+			clientP.Send (message, message.Length);
+
+		} catch(SocketException e){
+			Debug.Log ("no udp response: " + e);
+		}
+
+	}
+}
